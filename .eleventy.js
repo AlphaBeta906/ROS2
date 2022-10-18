e393880893a850c6029fd0b9a7474fa492db1b62
AlphaBeta906 <dragonkingalyx@gmail.com>
2022-10-18 08:03:28 +0800

    fix: fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,26 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy('./src/assets/font');
   eleventyConfig.addPassthroughCopy('./src/assets/img');
 
   eleventyConfig.addPassthroughCopy("./src/assets/style/main.css");
-  eleventyConfig.addPassthroughCopy({ "./src/_fonts": "/fonts" });
+  eleventyConfig.addPassthroughCopy("./src/assets/fonts");
 
   const {
     DateTime
   } = require("luxon");
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('yy-MM-dd');
+  });
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
+  eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {
       zone: 'utc'
     }).toFormat("DDD");
